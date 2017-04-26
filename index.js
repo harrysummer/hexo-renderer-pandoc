3c208668234a51b2abf3c875f3521e72df2f4d38
Harry Summer <harrysummerxr@gmail.com>
2017-04-26 15:50:29 +0800

    support both sync and async render, support multiple formats

--- index.js
+++ index.js
@@ -1,90 +1,117 @@
 var spawn = require('child_process').spawn;
+var spawnSync = require('child_process').spawnSync;
 
-var pandocRenderer = function(data, options, callback){
-  var config = hexo.config.pandoc;
-  var filters = [], extra = [], meta = [], math = '--mathjax';
+var config = hexo.config.pandoc;
+var filters = [], extra = [], meta = [], math = '--mathjax';
+if(config) {
+  if(config.filters) {
+    config.filters.forEach(function(filter) {
+      filters.push('--filter');
+      filters.push(filter);
+    });
+  }
 
-  if(config) {
-    if(config.filters) {
-      config.filters.forEach(function(filter) {
-        filters.push('--filter');
-        filters.push(filter);
-      });
-    }
-
-    if(config.extra) {
-      for(var e in config.extra) {
-        extra.push('--' + e);
-        extra.push(config.extra[e]);
-      }
-    }
-
-    if(config.meta) {
-      config.meta.forEach(function(m) {
-        meta.push('-M');
-        if(m.length) {
-          meta.push(m);
-        } else {
-          for(var m2 in m) {
-            meta.push(m2 + '=' + m[m2]);
-          }
-        }
-      });
-    }
-
-    if(config.mathEngine) {
-      if(typeof config.mathEngine === 'string') {
-        math = '--' + config.mathEngine;
-      }
+  if(config.extra) {
+    for(var e in config.extra) {
+      extra.push('--' + e);
+      extra.push(config.extra[e]);
     }
   }
 
-	var args = [ '-f', 'markdown', '-t', 'html', math, '--smart']
-  .concat(filters)
-  .concat(extra)
-  .concat(meta);
+  if(config.meta) {
+    config.meta.forEach(function(m) {
+      meta.push('-M');
+      if(m.length) {
+        meta.push(m);
+      } else {
+        for(var m2 in m) {
+          meta.push(m2 + '=' + m[m2]);
+        }
+      }
+    });
+  }
 
-	var src = data.text.toString();
+  if(config.mathEngine) {
+    if(typeof config.mathEngine === 'string') {
+      math = '--' + config.mathEngine;
+    }
+  }
+};
 
-	var pandoc = spawn('pandoc', args);
+var pandocRenderer = function(format) {
+  return function(data, options, callback){
+    var args = [ '-f', format, '-t', 'html', math, '--smart']
+      .concat(filters)
+      .concat(extra)
+      .concat(meta);
 
-	var result = '';
-	var error = '';
+    var src = data.text.toString();
 
-	pandoc.stdout.setEncoding('utf8');
+    var pandoc = spawn('pandoc', args);
 
-	pandoc.stdout.on('data', function (data) {
-		result += data.toString();
-	});
+    var result = '';
+    var error = '';
 
-	pandoc.stderr.on('data', function (data) {
-		error += data.toString();
-	});
+    pandoc.stdout.setEncoding('utf8');
 
-	pandoc.stdin.write(src, 'utf8');
+    pandoc.stdout.on('data', function (data) {
+      result += data.toString();
+    });
 
-	pandoc.on('close', function (code, signal) {
-		var msg = '';
-		if (code !== 0)
-			msg += 'pandoc exited with code '+code+(error ? ': ' : '.');
-		if (error)
-			msg += error;
-		if (msg)
-			return callback(new Error(msg));
-		else{
-			if (result === '') console.log("The next file error: ");
-			callback(null, result);
-		}
-	});
+    pandoc.stderr.on('data', function (data) {
+      error += data.toString();
+    });
+
+    pandoc.stdin.write(src, 'utf8');
+
+    pandoc.on('close', function (code, signal) {
+      var msg = '';
+      if (code !== 0)
+        msg += 'pandoc exited with code '+code+(error ? ': ' : '.');
+      if (error)
+        msg += error;
+      if (msg)
+        return callback(new Error(msg));
+      else{
+        if (result === '') console.log("The next file error: ");
+        callback(null, result);
+      }
+    });
 
     pandoc.stdin.end();
+  }
+}
+
+var pandocRendererSync = function(format) {
+  return function(data, options){
+    var args = [ '-f', format, '-t', 'html', math, '--smart']
+      .concat(filters)
+      .concat(extra)
+      .concat(meta);
+
+    var src = data.text.toString();
+
+    var pandoc = spawnSync('pandoc', args, { input: src, encoding: 'utf8' });
+
+      var msg = '';
+      if (pandoc.status !== 0)
+        msg += 'pandoc exited with code '+code+(pandoc.stderr ? ': ' : '.');
+      if (pandoc.stderr)
+        msg += pandoc.stderr;
+      if (msg)
+        return msg;
+      else{
+        if (pandoc.stdout === '') console.log("The next file error: ");
+        return pandoc.stdout;
+      }
+
+  }
 
 }
 
-hexo.extend.renderer.register('md', 'html', pandocRenderer);
-hexo.extend.renderer.register('markdown', 'html', pandocRenderer);
-hexo.extend.renderer.register('mkd', 'html', pandocRenderer);
-hexo.extend.renderer.register('mkdn', 'html', pandocRenderer);
-hexo.extend.renderer.register('mdwn', 'html', pandocRenderer);
-hexo.extend.renderer.register('mdtxt', 'html', pandocRenderer);
-hexo.extend.renderer.register('mdtext', 'html', pandocRenderer);
+hexo.extend.renderer.register('md', 'html', pandocRenderer('markdown'));
+hexo.extend.renderer.register('md', 'html', pandocRendererSync('markdown'), true);
+hexo.extend.renderer.register('mmd', 'html', pandocRenderer('markdown_mmd'));
+hexo.extend.renderer.register('mmd', 'html', pandocRendererSync('markdown_mmd'), true);
+hexo.extend.renderer.register('mw', 'html', pandocRenderer('mediawiki'));
+hexo.extend.renderer.register('mw', 'html', pandocRendererSync('mediawiki'), true);
